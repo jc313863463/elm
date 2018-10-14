981c1dfe1e0358bcd29e0ba0e7b76f03963cdfc4
jc313863463 <3138634637@qq.com>
2018-10-14 16:16:50 +0800

    add resource

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -12,7 +12,17 @@ const portfinder = require('portfinder')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
-
+const express=require('express')
+const app=express()
+var appData=require('../data.json')
+var seller=appData.seller
+var goods=appData.goods
+var ratings = appData.ratings
+            // 编写路由
+            var apiRoutes = express.Router()
+            // 所有通过接口相关的api都会通过api这个路由导向到具体的路由
+            app.use('/api', apiRoutes)
+            // 找到devServer对象并在其后添加相关路由设置
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
@@ -42,6 +52,29 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before (app) {
+      app.get('/api/seller', function (req, res) {
+        // 服务端收到请求后返回给客户端一个json数据
+        res.json({
+          // 当我们数据正常时，我们通过传递errno字符为0表示数据正常
+          errno: 0,
+          // 返回json中的卖家数据
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (rea, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   },
   plugins: [
